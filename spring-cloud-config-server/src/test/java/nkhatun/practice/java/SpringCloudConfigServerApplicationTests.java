2824107909a03d316bd0c66a9be42bd92997198c
Najma Khatun <GTFS.Najma@DPWORLD.AE>
2021-05-18 14:28:16 +0400

    spring cloud server project

---
+++ spring-cloud-config-server/src/test/java/nkhatun/practice/java/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nkhatun.practice.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
